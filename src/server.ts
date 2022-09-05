5f7977dc1ba7cac7ee5d0a1036ea9097bc3f8176
Leon <junzai12@icloud.com>
2022-09-05 12:43:00 -0700

    trying api again

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get('/api/v1', (req, res) => {
+    res.send({"Generation One:": "/api/v1/GenerationOne"})
+});
+
+app.get("*")
+
+app.listen(3000)
